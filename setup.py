6e69e3cc201b3bdfe77fc7af12656e0b4b3f1562
Lakshay Goel <iamlakshaygoel5990@gmail.com>
2025-06-30 16:26:29 +0530

    adding exception and logging file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="lakshay",
+    author_email="iamlakshaygoel5990gmail.com",
+    packages=find_packages()
+)
